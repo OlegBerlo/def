93b88bda79209e0a2e35332dcb29754d77979bbf
Oleg <oleg.berlo@ukr.net>
2023-06-08 08:22:24 +0300

    lesson-2

---
+++ src/Container/Main/Main.tsx
@@ -0,0 +1,15 @@
+import { Container } from "@mui/material";
+
+type Props = {};
+const Main = (props: Props) => {
+  return (
+    <Container
+      sx={{
+        padding: "40px 0",
+      }}
+    >
+      Main
+    </Container>
+  );
+};
+export default Main;
