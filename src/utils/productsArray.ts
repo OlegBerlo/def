ba02ad373e3df1afe27fd0309ab38069d65bc758
Oleg <oleg.berlo@ukr.net>
2023-06-10 13:54:00 +0300

    added productsArray in ProductList

---
+++ src/utils/productsArray.ts
@@ -0,0 +1,76 @@
+type Product = {
+  title: string
+  description: string
+  capacity: string
+  type: string
+  price:number
+}
+
+const productsArray:Product[] = [
+{
+  title: 'iPhone 14 Pro',
+  description: 'This is iPhone 14 Pro',
+  type: 'phone',
+  capacity: '256',
+  price: 1500,
+},
+{
+  title: 'iPhone 14 Pro',
+  description: 'This is iPhone 14 Pro',
+  type: 'phone',
+  capacity: '256',
+  price: 1500,
+},
+{
+  title: 'iPhone 14 Pro',
+  description: 'This is iPhone 14 Pro',
+  type: 'phone',
+  capacity: '256',
+  price: 1500,
+},
+{
+  title: 'iPhone Nokia Pro',
+  description: 'This is Bybkya 14 Pro',
+  type: 'phone',
+  capacity: '218273',
+  price: 1,
+},
+{
+  title: 'iPhone Keglya Pro',
+  description: 'This is iPhone 19273 Pro',
+  type: 'phone',
+  capacity: '252',
+  price: 80,
+},
+{
+  title: 'iPhone Mipka Pro',
+  description: 'This is iPhone :) Pro',
+  type: 'phone',
+  capacity: '8',
+  price: 1,
+},
+{
+  title: 'iPhone Nokia Pro',
+  description: 'This is Bybkya 14 Pro',
+  type: 'phone',
+  capacity: '218273',
+  price: 1,
+},
+{
+  title: 'iPhone Keglya Pro',
+  description: 'This is iPhone 19273 Pro',
+  type: 'phone',
+  capacity: '252',
+  price: 80,
+},
+{
+  title: 'iPhone Mipka Pro',
+  description: 'This is iPhone :) Pro',
+  type: 'phone',
+  capacity: '8',
+  price: 1,
+}
+]
+
+
+export default productsArray
