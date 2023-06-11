ca0d4f4d38af37f74005b54afbdb8cb25936759a
Oleg <oleg.berlo@ukr.net>
2023-06-11 08:01:03 +0300

    product kyy

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import ProductList from "components/Products/ProductList";
-
+import "./Home.scss";
 type Props = {};
 const Home = (props: Props) => {
   return (
--- src/utils/productsArray.ts
+++ src/utils/productsArray.ts
@@ -1,4 +1,5 @@
 type Product = {
+  id:number
   title: string
   description: string
   capacity: string
@@ -8,6 +9,7 @@ type Product = {
 
 const productsArray:Product[] = [
 {
+  id:1,
   title: 'iPhone 14 Pro',
   description: 'This is iPhone 14 Pro',
   type: 'phone',
@@ -15,6 +17,7 @@ const productsArray:Product[] = [
   price: 1500,
 },
 {
+  id:2,
   title: 'iPhone 14 Pro',
   description: 'This is iPhone 14 Pro',
   type: 'phone',
@@ -22,6 +25,7 @@ const productsArray:Product[] = [
   price: 1500,
 },
 {
+  id:3,
   title: 'iPhone 14 Pro',
   description: 'This is iPhone 14 Pro',
   type: 'phone',
@@ -29,13 +33,15 @@ const productsArray:Product[] = [
   price: 1500,
 },
 {
+  id:4,
   title: 'iPhone Nokia Pro',
   description: 'This is Bybkya 14 Pro',
   type: 'phone',
   capacity: '218273',
-  price: 1,
+  price: 10,
 },
 {
+  id:5,
   title: 'iPhone Keglya Pro',
   description: 'This is iPhone 19273 Pro',
   type: 'phone',
@@ -43,20 +49,23 @@ const productsArray:Product[] = [
   price: 80,
 },
 {
+  id:6,
   title: 'iPhone Mipka Pro',
   description: 'This is iPhone :) Pro',
   type: 'phone',
   capacity: '8',
-  price: 1,
+  price: 991,
 },
 {
+  id:7,
   title: 'iPhone Nokia Pro',
   description: 'This is Bybkya 14 Pro',
   type: 'phone',
   capacity: '218273',
-  price: 1,
+  price: 881,
 },
 {
+  id:8,
   title: 'iPhone Keglya Pro',
   description: 'This is iPhone 19273 Pro',
   type: 'phone',
@@ -64,11 +73,12 @@ const productsArray:Product[] = [
   price: 80,
 },
 {
+  id:9,
   title: 'iPhone Mipka Pro',
   description: 'This is iPhone :) Pro',
   type: 'phone',
   capacity: '8',
-  price: 1,
+  price: 177,
 }
 ]
 
