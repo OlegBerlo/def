53b25ef1cde39b6cbe818ae06c212aeae47283b2
Oleg <oleg.berlo@ukr.net>
2023-06-11 14:40:18 +0300

    set

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import {
   Button,
   Card,
@@ -16,70 +16,42 @@ type Props = {
   price: number;
   image: string;
 };
-type State = {
-  count: number;
-  color: string;
+
+const ProductListItem = ({
+  title,
+  description,
+  type,
+  capacity,
+  price,
+  image,
+}: Props) => {
+  const [count, setCount] = useState<number>(5);
+  return (
+    <Card className="product" variant="outlined">
+      <CardContent>
+        <div className="product-img">
+          <img src={image} alt="logo" />
+        </div>
+        <h4 className="product-title">{title}</h4>
+        <div className="product-description">{description}</div>
+        <div className="product-features">Type: {type}</div>
+        <div className="product-features">Capacity: {capacity} Gb</div>
+        <div className="product-price">Price: $ {price}</div>
+        <div className="product-quantity">
+          <Button variant="outlined" onClick={() => setCount(count - 1)}>
+            -
+          </Button>
+          <TextField value={count} size="small" />
+          <Button variant="outlined" onClick={() => setCount(count + 1)}>
+            +
+          </Button>
+        </div>
+        <CardActions className="btn-wrap">
+          <Button variant="outlined">Add to cart</Button>
+        </CardActions>
+      </CardContent>
+    </Card>
+  );
 };
-class ProductListItem extends React.Component<Props, State> {
-  state = {
-    count: 1,
-    color: "green",
-  };
-  onIncrementClick = () => {
-    this.setState((prevState) => ({
-      count: prevState.count + 1,
-    }));
-  };
-  onDecrementClick = () => {
-    this.setState((prevState) => ({
-      count: prevState.count - 1,
-    }));
-  };
 
-  changeColor = () => {
-    this.setState((prevState) => ({
-      color: prevState.color === "green" ? "red" : "green",
-    }));
-  };
-
-  render() {
-    const { title, description, type, capacity, price, image } = this.props;
-    return (
-      <Card className="product" variant="outlined">
-        <CardContent>
-          <div className="product-img">
-            <img src={image} alt="logo" />
-          </div>
-          <h4 className="product-title">{title}</h4>
-          <div className="product-description">{description}</div>
-          <div className="product-features">Type: {type}</div>
-          <div className="product-features">Capacity: {capacity} Gb</div>
-          <div>
-            Color:{" "}
-            <span className={`${this.state.color}`}>{this.state.color}</span>
-          </div>
-          <button onClick={() => this.changeColor()}>Change color</button>
-          <div className="product-price">Price: $ {price}</div>
-          <div className="product-quantity">
-            <Button
-              variant="outlined"
-              onClick={() => this.onDecrementClick()}
-              disabled={this.state.count <= 1}
-            >
-              -
-            </Button>
-            <TextField value={this.state.count} size="small" />
-            <Button
-              variant="outlined"
-              onClick={() => this.onIncrementClick()}
-              disabled={this.state.count >= 10}
-            >
-              +
-            </Button>
-          </div>
-        </CardContent>
-      </Card>
-    );
-  }
-}
 export default ProductListItem;
