c37befeb7df2916318fc4907395cbd42ffb7c466
Oleg <oleg.berlo@ukr.net>
2023-06-11 15:21:01 +0300

    color

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -25,7 +25,20 @@ const ProductListItem = ({
   price,
   image,
 }: Props) => {
-  const [count, setCount] = useState<number>(5);
+  const [count, setCount] = useState<number>(1);
+  const [color, setColor] = useState<string>("green");
+
+  const onIncrementClick = () => {
+    setCount((prevState) => prevState + 1);
+  };
+  const onDecrementClick = () => {
+    setCount((prevState) => prevState - 1);
+  };
+
+  const changeColor = () => {
+    setColor((prevState) => (prevState === "green" ? "red" : "green"));
+  };
+
   return (
     <Card className="product" variant="outlined">
       <CardContent>
@@ -36,13 +49,23 @@ const ProductListItem = ({
         <div className="product-description">{description}</div>
         <div className="product-features">Type: {type}</div>
         <div className="product-features">Capacity: {capacity} Gb</div>
+        <div>Color:{color}</div>
+        <button onClick={() => changeColor()}>Change color</button>
         <div className="product-price">Price: $ {price}</div>
         <div className="product-quantity">
-          <Button variant="outlined" onClick={() => setCount(count - 1)}>
+          <Button
+            variant="outlined"
+            onClick={() => onDecrementClick()}
+            disabled={count <= 1}
+          >
             -
           </Button>
           <TextField value={count} size="small" />
-          <Button variant="outlined" onClick={() => setCount(count + 1)}>
+          <Button
+            variant="outlined"
+            onClick={() => onIncrementClick()}
+            disabled={count >= 10}
+          >
             +
           </Button>
         </div>
