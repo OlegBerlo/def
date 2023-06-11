26a26dd67fa9dc4a39e18376fd26d058a9cbac14
Oleg <oleg.berlo@ukr.net>
2023-06-11 10:46:26 +0300

    this

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -7,6 +7,7 @@ import {
   TextField,
 } from "@mui/material";
 import "./ProductListItem.scss";
+import { type } from "os";
 type Props = {
   title: string;
   description: string;
@@ -15,30 +16,48 @@ type Props = {
   price: number;
   image: string;
 };
+type State = {
+  count: number;
+};
+class ProductListItem extends React.Component<Props, State> {
+  state = {
+    count: 1,
+  };
+  onIncrementClick = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+  onDecrementClick = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+  };
 
-class ProductListItem extends React.Component<Props> {
   render() {
+    const { title, description, type, capacity, price, image } = this.props;
     return (
       <Card className="product" variant="outlined">
         <CardContent>
           <div className="product-img">
-            <img src={this.props.image} alt="logo" />
+            <img src={image} alt="logo" />
           </div>
-          <h4 className="product-title">{this.props.title}</h4>
-          <div className="product-description">{this.props.description}</div>
-          <div className="product-features">Type: {this.props.type}</div>
+          <h4 className="product-title">{title}</h4>
+          <div className="product-description">{description}</div>
+          <div className="product-features">Type: {type}</div>
           <div className="product-features">
             Capacity: {this.props.capacity} Gb
           </div>
-          <div className="product-price">Price: $ {this.props.price}</div>
+          <div className="product-price">Price: $ {price}</div>
           <div className="product-quantity">
-            <Button variant="outlined">-</Button>
-            <TextField value="1" size="small" />
-            <Button variant="outlined">+</Button>
+            <Button variant="outlined" onClick={() => this.onDecrementClick()}>
+              -
+            </Button>
+            <TextField value={this.state.count} size="small" />
+            <Button variant="outlined" onClick={() => this.onIncrementClick()}>
+              +
+            </Button>
           </div>
-          <CardActions className="btn-wrap">
-            <Button variant="outlined">Add to cart</Button>
-          </CardActions>
         </CardContent>
       </Card>
     );
